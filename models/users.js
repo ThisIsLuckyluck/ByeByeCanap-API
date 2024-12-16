350586871783b86d4b2e9291cab5f8756ee0ffaf
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-12-16 21:22:07 +0100

    adding checkbody to see body's content and creating users model + schema and adding users routes

---
+++ models/users.js
@@ -0,0 +1,59 @@
+// -------------------- Import Zone --------------------
+const mongoose = require("mongoose");
+// -------------------- end of Import Zone --------------------
+
+// -------------------- start of the embedded documents part --------------------
+const aspirationsSchema = mongoose.Schema({
+  themesInterest: { type: [String], default: [] },
+  categoriesInterest: { type: [String], default: [] },
+  themesSkill: { type: [String], default: [] },
+  categoriesSkill: { type: [String], default: [] },
+});
+
+const availabilitySchema = mongoose.Schema({
+  availability: { type: [String], default: [] },
+  locationPreference: { type: String, default: null },
+});
+
+const valuesSchema = mongoose.Schema({
+  preferredPeople: { type: [String], default: [] },
+  preferredGroupType: { type: [String], default: [] },
+  personalValues: { type: [String], default: [] },
+  causes: { type: [String], default: [] },
+});
+// -------------------- end of embedded documents part --------------------
+
+// -------------------- start of the Schema's Zone --------------------
+const usersSchema = new mongoose.Schema({
+  // Personnal Informations
+  username: String,
+  email: String,
+  password: String,
+  token: String,
+  userType: String,
+
+  // Public Information
+  lastname: String,
+  firstname: String,
+  age: Number,
+  birthDate: Date,
+  gender: String,
+  avatar: String,
+  descriptionProfile: String,
+  isProfileChecked: Boolean,
+  inscriptionDate: Date,
+
+  // Embedded document import
+  aspirations: { type: aspirationsSchema, default: {} },
+  availability: { type: availabilitySchema, default: {} },
+  values: { type: valuesSchema, default: {} },
+});
+// -------------------- end of the Schema's Zone --------------------
+
+// -------------------- Model's Zone --------------------
+const users = mongoose.model("users", usersSchema);
+// -------------------- end of Model's Zone --------------------
+
+// -------------------- Export Zone --------------------
+module.exports = users;
+// -------------------- Export Zone --------------------
