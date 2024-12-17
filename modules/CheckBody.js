5c5af9363ad5aa1a93c6e43cf3cfc973cb464d11
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-12-17 01:41:09 +0100

    updating CheckBody by removing optionalfields and removing index.js files

--- modules/CheckBody.js
+++ modules/CheckBody.js
@@ -1,14 +1,7 @@
 function CheckBody(body, requiredFields, optionalFields = []) {
-  // Vérification des champs obligatoires
   for (let field of requiredFields) {
     if (!body[field] || body[field].trim() === "") {
       return false;
-    }
-  } // Initialisation des champs optionnels
-
-  for (let field of optionalFields) {
-    if (!body[field]) {
-      body[field] = Array.isArray(body[field]) ? [] : null;
     }
   }
 
